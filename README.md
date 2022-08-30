c0fcd015387de2b256622d14473a9714e077f5d6
nik2704 <nik2704@yandex.ru>
2022-08-30 19:04:11 +0300

    Tests without conditions and Readme was prepared

--- README.md
+++ README.md
@@ -7,3 +7,78 @@
 
 <a href="https://codeclimate.com/github/nik2704/java-project-lvl3/test_coverage"><img src="https://api.codeclimate.com/v1/badges/afb66a0d8d00ae36edc0/test_coverage" /></a>
 
+# **Валидатор данных**
+
+**Предназначен для проверки значений различных типов на соотвестствие требованиям.**
+
+**Поддерживаемые типы данных:**
+* Строка
+* Целое число
+* Ассоциативный массив со строковыми ключами и значениями одного из поддерживаемых типов.
+
+**Доступные методы:**
+1) Строки
+   required – любая непустая строка
+   minLength – строка равна или длиннее указанного числа
+   contains – строка содержит определённую подстроку
+2) Числа
+   required – любое число включая ноль
+   positive – положительное число
+   range – диапазон, в который должны попадать числа включая границы
+3) Ассоциативные массивы
+   required – требуется тип данных Map
+   sizeof – количество пар ключ-значений в объекте Map должно быть равно заданному
+
+Примеры использования:
+
+1) Валидация строк
+
+
+    import hexlet.code.Validator;
+    import hexlet.code.schemas.StringSchema;
+    Validator v = new Validator();
+    StringSchema schema = v.string();
+    
+    schema.isValid(""); // true
+    schema.isValid(null); // true
+    
+    schema.required();
+    
+    schema.isValid("what does the fox say"); // true
+    schema.isValid("hexlet"); // true
+    schema.isValid(null); // false
+    schema.isValid(""); // false
+    
+    schema.contains("wh").isValid("what does the fox say"); // true
+    schema.contains("what").isValid("what does the fox say"); // true
+    schema.contains("whatthe").isValid("what does the fox say"); // false
+    
+    schema.isValid("what does the fox say"); // false
+    // уже false, так как добавлена ещё одна проверка contains("whatthe")
+
+
+2) Валидация ассоциативного массива
+
+
+    import hexlet.code.Validator;
+    import hexlet.code.schemas.MapSchema;
+    
+    Validator v = new Validator();
+    
+    MapSchema schema = v.map();
+    
+    schema.isValid(null); // true
+    
+    schema.required();
+    
+    schema.isValid(null) // false
+    schema.isValid(new HashMap()); // true
+    Map<String, String> data = new HashMap<>();
+    data.put("key1", "value1");
+    schema.isValid(data); // true
+    
+    schema.sizeof(2);
+    
+    schema.isValid(data);  // false
+    data.put("key2", "value2");
+    schema.isValid(data); // true
--- src/test/java/hexlet/code/StringSchemaTest.java
+++ src/test/java/hexlet/code/StringSchemaTest.java
@@ -1,76 +1,50 @@
 package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public final class StringSchemaTest {
+    private final int v0 = 0;
+    private final int v5 = 5;
+    private final int v6 = 6;
+    private final int v7 = 7;
 
-    @ParameterizedTest
-    @CsvSource(value = {
-        "stroka, default, 0, true",
-        "BLANC, default, 0, true",
-        "NULL, default, 0, true",
-        "stroka, REQUIRED, 0, true",
-        "BLANC, REQUIRED, 0, false",
-        "NULL, REQUIRED, 0, false",
-        "stroka, MIN, 6, true",
-        "stroka, MIN, 5, true",
-        "stroka, MIN, 7, false",
-        "BLANC, MIN, 7, false",
-//        "NULL, MIN, 7, false",
-        "NULL, MIN, 0, true",
-        "WHITESPACE, REQUIRED, 0, true",
-        "WHITESPACE, MIN, 1, true",
-        "WHITESPACE, MIN, 0, true",
-        "stroka, CONTAINS, 5, true",
-        "STROKA, CONTAINS, 5, false",
-        "BLANC, CONTAINS, 5, false",
-        "stroka, CONTAINS+MIN, 5, true",
-        "ooko, CONTAINS+MIN, 5, false"
-        }, ignoreLeadingAndTrailingWhitespace = true)
-    public void testValue(String value, String mode, int minLength, boolean expected) {
+    @Test
+    public void testValue() {
+        Validator v = new Validator();
+
+        StringSchema schema = v.string();
+
+        assertThat(schema.isValid("")).isEqualTo(true);
+        assertThat(schema.isValid(null)).isEqualTo(true);
+
+        assertThat(schema.required());
+
+        assertThat(schema.isValid("what does the fox say")).isEqualTo(true);
+        assertThat(schema.isValid("hexlet")).isEqualTo(true);
+        assertThat(schema.isValid(null)).isEqualTo(false);
+        assertThat(schema.isValid("")).isEqualTo(false);
+
+        assertThat(schema.contains("wh").isValid("what does the fox say")).isEqualTo(true);
+        assertThat(schema.contains("what").isValid("what does the fox say")).isEqualTo(true);
+        assertThat(schema.contains("whatthe").isValid("what does the fox say")).isEqualTo(false);
+
+        assertThat(schema.isValid("what does the fox say")).isEqualTo(false);
+    }
+
+    @Test
+    public void testLengthValue() {
         Validator v = new Validator();
         StringSchema schema = v.string();
-        boolean controlData = false;
-        String testValue = value;
 
-        switch (value) {
-            case "BLANC":
-                testValue = "";
-                break;
-            case "NULL":
-                testValue = null;
-                break;
-            case "WHITESPACE":
-                testValue = " ";
-                break;
-            default:
-        }
-
-        switch (mode) {
-            case "default":
-                controlData = schema.isValid(testValue);
-                break;
-            case "REQUIRED":
-                controlData = schema.required().isValid(testValue);
-                break;
-            case "MIN":
-                controlData = schema.minLength(minLength).isValid(testValue);
-                break;
-            case "CONTAINS":
-                controlData = schema.contains("ok").isValid(testValue);
-                break;
-            case "CONTAINS+MIN":
-                controlData = schema.minLength(minLength).contains("ok").isValid(testValue);
-                break;
-            default:
-        }
-
-        assertThat(controlData).isEqualTo(expected);
+        assertThat(schema.minLength(v6).isValid("stroka")).isEqualTo(true);
+        assertThat(schema.minLength(v5).isValid("stroka")).isEqualTo(true);
+        assertThat(schema.minLength(v7).isValid("stroka")).isEqualTo(false);
+        assertThat(schema.minLength(v7).isValid(" ")).isEqualTo(false);
+        assertThat(schema.minLength(v0).isValid(null)).isEqualTo(true);
     }
+
 }
